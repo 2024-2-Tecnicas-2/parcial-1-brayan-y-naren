ecba230a652dcd7a273dc1caf0ea5727ebb651ba
jhondoe <jhondoe@gmail.com>
2024-09-16 20:27:05 -0500

    Scanner

--- src/main/java/com/mycompany/parcial1/Principal.java
+++ src/main/java/com/mycompany/parcial1/Principal.java
@@ -1,8 +1,10 @@
 package com.mycompany.parcial1;
 
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
+public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO: Aquí ingresas tu código para iniciar la aplicación.
+        
     }
 }
