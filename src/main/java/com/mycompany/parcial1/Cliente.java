e80514981191ca969e30145b9615d8e8af30a960
jonuits <apple@joulits.com>
2024-09-16 20:23:12 -0500

    Clase cliente

--- src/main/java/com/mycompany/parcial1/Cliente.java
+++ src/main/java/com/mycompany/parcial1/Cliente.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.parcial1;
 
-/**
- *
- * @author USUARIO
- */
+
 public class Cliente {
-    
+   String nombre;
+   String direccion;
+   String listaPedidos;
+
+    public Cliente(String nombre, String direccion, String listaPedidos) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.listaPedidos = listaPedidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getListaPedidos() {
+        return listaPedidos;
+    }
+
+    public void setListaPedidos(String listaPedidos) {
+        this.listaPedidos = listaPedidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", direccion=" + direccion + ", listaPedidos=" + listaPedidos + '}';
+    }
+   
 }
