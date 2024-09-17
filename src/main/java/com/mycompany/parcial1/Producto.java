85a75c29010293fdf37dd32f7e8eeaf0edc19f5c
jhondoe <jhondoe@gmail.com>
2024-09-16 20:28:35 -0500

    Create get and set

--- src/main/java/com/mycompany/parcial1/Producto.java
+++ src/main/java/com/mycompany/parcial1/Producto.java
@@ -1,9 +1,49 @@
 package com.mycompany.parcial1;
 
 public abstract class Producto {
-    public String nombre;
-    public String Descripcion;
-    public int inventario;
-    public double precio;
+    private String nombre;
+    private String Descripcion;
+    private int inventario;
+    private double precio;
+
+    public Producto(String nombre, String Descripcion, int inventario, double precio) {
+        this.nombre = nombre;
+        this.Descripcion = Descripcion;
+        this.inventario = inventario;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public int getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(int inventario) {
+        this.inventario = inventario;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
     
 }
